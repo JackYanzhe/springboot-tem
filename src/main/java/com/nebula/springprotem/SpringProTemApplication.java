b2fd4681aeac806a6db4fd779fb11000ac3d5853
JackYanzhe <532968876@qq.com>
2021-03-28 11:08:13 +0800

    springboot模板

---
+++ src/main/java/com/nebula/springprotem/SpringProTemApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.springprotem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProTemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProTemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nebula/springprotem/SpringProTemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.springprotem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProTemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
